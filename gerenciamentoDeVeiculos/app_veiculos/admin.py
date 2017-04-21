4808be5a7b8844e7720909ced5e8a21f60b5ec14
Daiane-Martins <cmsdaiane@gmail.com>
2017-04-20 21:02:41 -0400

    Gerenciamento de veículos  1.0

---
+++ gerenciamentoDeVeiculos/app_veiculos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from app_veiculos.models import TipoDeVeiculo, MarcaDoVeiculo,Veiculo
+
+admin.site.register(TipoDeVeiculo)
+admin.site.register(MarcaDoVeiculo)
+admin.site.register(Veiculo)
